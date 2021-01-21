a381f2a1faea22b37bd768bff5b58d8243660dcf
amaurysoares <amauryrodrigo@gmail.com>
2021-01-21 02:11:23 -0300

    Laravel passport is now protecting Api access from non logged users

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,4 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('operators', 'api\CreditCardOperatorController');
+Route::post('/register', 'Api\AuthController@register');
+
+Route::post('/login', 'Api\AuthController@login');
+
+Route::apiResource('operators', 'Api\CreditCardOperatorController')->middleware('auth:api');;
